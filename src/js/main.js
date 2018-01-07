83ca6008d241b0ebf282dd66b66b0b4ee9837aa8
Unknown <leszek@lubcon.org>
2018-01-07 12:01:36 +0100

    Add FullCalnder

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
+"use strict";
+$(document).ready(function () {
+    $(".calendar").fullCalendar({
+        weekends: false,
+        googleCalendarApiKey: "AIzaSyCbmM8xX6md1IjtFGGXTgDjSA9iNI34_-k",
+        events: {
+            googleCalendarId: "lubcon.org_qi5qeu5bm83puvc9kfj9mfle74@group.calendar.google.com",
+        },
+        height: 500,
 
+    });
+
+});
--- tasks/config.js
+++ tasks/config.js
@@ -26,6 +26,10 @@ module.exports = {
             "./node_modules/jquery/dist/jquery.min.js",
             "./node_modules/popper.js/dist/umd/popper.min.js",
             "./node_modules/bootstrap/dist/js/bootstrap.min.js",
+            "./node_modules/moment/min/moment.min.js",
+            "./node_modules/fullcalendar/dist/fullcalendar.js",
+            "./node_modules/fullcalendar/dist/gcal.js",
+            "./node_modules/fullcalendar/dist/locale/pl.js",
         ]
     },
     name: {
